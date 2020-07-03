0264a95bc462c7a20aa759586322d9daa370daa5
田中蓮 <ren_kiyo_sao@icloud.com>
2020-07-04 03:17:22 +0900

    スタートボタンの点滅

---
+++ puroto2/Assets/Script/Title/start.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private Image image;
+    private float time;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.color = GetAlphaColor(image.color);
+    }
+
+    private Color GetAlphaColor(Color color)
+    {
+        time += Time.deltaTime * 5.0f * speed;
+        color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+        return color;
+    }
+}
